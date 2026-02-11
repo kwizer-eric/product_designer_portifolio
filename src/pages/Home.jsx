e8ef0cd4714e86f5d61a3f8c3c6f4134546819f0
eric <kwizeraeric312@gmail.com>
2026-02-11 18:49:29 +0200

    red

---
+++ src/pages/Home.jsx
@@ -0,0 +1,50 @@
+
+import { useEffect } from 'react';
+import Lenis from '@studio-freight/lenis';
+import Hero from '../components/Hero';
+import Story from '../components/Story';
+import Process from '../components/Process';
+import Work from '../components/Work';
+import Gallery from '../components/Gallery';
+import Services from '../components/Services';
+import Testimonials from '../components/Testimonials';
+import Footer from '../components/Footer';
+
+export default function Home() {
+    useEffect(() => {
+        const lenis = new Lenis({
+            duration: 1.2,
+            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+            direction: 'vertical',
+            gestureDirection: 'vertical',
+            smooth: true,
+            mouseMultiplier: 1,
+            smoothTouch: false,
+            touchMultiplier: 2,
+        });
+
+        function raf(time) {
+            lenis.raf(time);
+            requestAnimationFrame(raf);
+        }
+
+        requestAnimationFrame(raf);
+
+        return () => {
+            lenis.destroy();
+        };
+    }, []);
+
+    return (
+        <div className="home-page">
+            <Hero />
+            <Story />
+            <Services />
+            <Process />
+            <Work />
+            <Gallery />
+            <Testimonials />
+            <Footer />
+        </div>
+    );
+}
